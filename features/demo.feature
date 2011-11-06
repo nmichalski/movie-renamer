89c87548953b40076c84f430c088aad079f9d8de
Nick Michalski <Nicholas.A.Michalski@gmail.com>
2011-11-06 04:12:16 -0600

    Added another cucumber scenario for empty directories

--- features/demo.feature
+++ features/demo.feature
@@ -2,6 +2,10 @@ Feature: Demo
   Make sure Cucumber and Directory/File listing is working fine
 
   Scenario: List a movie
-    When I choose the movie directory "Movies"
+    When I choose the movie directory "test/Movies"
     Then there should be a listing for "foobar.avi"
+
+  Scenario: Movie directory is empty
+    When I choose an empty directory like "test/EmptyDirectory"
+    Then there should be a message saying "Movie directory was empty"
 
--- features/step_definitions/demo_steps.rb
+++ features/step_definitions/demo_steps.rb
@@ -1,9 +1,23 @@
+STARTING_DIRECTORY = Dir.pwd
+
 When /^I choose the movie directory "(.*)"$/ do |dir|
+  Dir.chdir(STARTING_DIRECTORY)
   Dir.chdir(dir)
   Dir.getwd[-dir.length, dir.length].should == dir
+end
+
+When /^I choose an empty directory like "(.*)"$/ do |dir|
+  Dir.chdir(STARTING_DIRECTORY)
+  Dir.chdir(dir)
+  Dir.getwd[-dir.length, dir.length].should == dir
+  Dir.entries(".").should == [".", ".."]
 end
 
 Then /^there should be a listing for "(.*)"$/ do |movie|
   Dir.entries(".").should include movie
 end
 
+Then /^there should be a message saying "(.*)"$/ do |msg|
+  #TODO: make MovieRenamer class
+  #MovieRenamer.error_message.should == msg
+end
